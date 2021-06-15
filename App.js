81e08c1e1645001b64dfb2e28232e6d78c499f81
Wagner Araújo <wagneraraujo15@gmail.com>
2021-06-15 09:00:19 -0300

    configure navigate and starting first screen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { store, persistor } from "./src/store";
-import { Text } from "react-native";
-
+import MainStack from "./src/navigators/MainStack";
 //import stack
+// preload => n aparece e define para onde envia o usuario
 
 export default () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Text>ola</Text>
+      <MainStack />
     </PersistGate>
   </Provider>
 );
